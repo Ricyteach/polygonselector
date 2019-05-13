16829031adb72b91b48fb7c50f16f0ab063fcdc4
Rick Teachey <ricky@teachey.org>
2019-05-13 16:58:39 -0400

    dummy package for importing the library

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('README.md') as readme_file:
 setup(
     name='polygonselector',
     version='0.1',
-    packages=find_packages(include=['notebook']),
+    packages=find_packages(include=['polygonselector']),
     url='https://github.com/Ricyteach/polygonselector',
     license='MIT',
     include_package_data=True,
     long_description=readme,
     author='Ricky L Teachey Jr',
     author_email='ricky@teachey.org',
-    description='A custom IPywidgets tool for graphically selecting shapely polygons inside a Jupyter notebook. '
+    description='A custom IPywidgets tool for graphically selecting shapely polygons inside a Jupyter notebooks. '
 )
