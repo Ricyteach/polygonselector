22934a111b89d857d7c47dfb64c9c69bb0f235e3
Rick Teachey <ricky@teachey.org>
2020-05-15 17:48:11 -0400

    add requirements, testing assets

--- polygonselector/init.py
+++ polygonselector/init.py
@@ -6,6 +6,8 @@ import fiona
 def open_(shp, df=None):
     features = list(fiona.open(shp))
     ids = [f["id"] for f in features]
+    if df is None:
+        df = pd.DataFrame(columns=["ID", ])
     geo_json = dict(type="FeatureCollection", features=features)
     fig = px.choropleth(geojson=geo_json, locations=ids, labels=dict(locations="e"))
     return fig
